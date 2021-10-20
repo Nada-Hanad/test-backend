117265e5f7e616e245667293f24bf887dc04313f
Hanad-Nada <jn_hanad@esi.dz>
2021-10-20 20:16:10 +0100

    master

--- config/db.ts
+++ config/db.ts
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose';
 
 mongoose
-  .connect('mongodb://localhost:27017/test')
+  .connect(
+    'mongodb+srv://dbPosts:passtoposts@cluster0.pnvk4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    { useNewUrlParser: true }
+  )
   .then(() => {
     console.log('Connected to DB');
   })
@@ -10,3 +13,24 @@ mongoose
   });
 
 module.exports = mongoose.connection;
+/*const { MongoClient } = require('mongodb');
+
+// Replace the following with your Atlas connection string
+const url =
+  'mongodb+srv://dbPosts:passtoposts@cluster0.pnvk4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const client = new MongoClient(url);
+const dbName = 'test';
+
+async function run() {
+  try {
+    await client.connect();
+    console.log('Connected correctly to server');
+    const db = client.db(dbName);
+    const col = db.collection('posts');
+  } catch (err: any) {
+    console.log(err.stack);
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);*/
